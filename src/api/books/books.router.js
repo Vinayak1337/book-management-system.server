9ad0ff5635d7332c4c3882119464d994324fb015
Vinayak1337 <34209962+Vinayak1337@users.noreply.github.com>
2024-01-09 03:22:19 +0530

    removing authorization from thumbnail

--- src/api/books/books.router.js
+++ src/api/books/books.router.js
@@ -16,6 +16,5 @@ booksRouter.route('/unpublish/:id').post(unpublishBookById);
 booksRouter.route('/user').get(getUserPublishedBooks);
 booksRouter.route('/published').get(getAllPublishedBooks);
 booksRouter.route('/search').get(searchBooksByTitle);
-booksRouter.route('/:id/thumbnail').get(getBookThumbnail);
 
 export default booksRouter;
--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -6,6 +6,7 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import config from './config.js';
 import fs from 'fs';
+import { getBookThumbnail } from '../api/books/books.controller.js';
 
 const swaggerConfig = JSON.parse(fs.readFileSync('./swagger.json', 'utf-8'));
 
@@ -23,6 +24,7 @@ const initMiddlewares = app => {
 	);
 	app.use(morgan('dev'));
 	app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
+	app.use('/api/books/:id/thumbnail', getBookThumbnail);
 };
 
 export default initMiddlewares;
