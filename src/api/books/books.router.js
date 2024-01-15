a8a060af01f7820060616841daf43b85a7a1834d
Vinayak1337 <34209962+Vinayak1337@users.noreply.github.com>
2024-01-16 02:32:22 +0530

    fixed unpublish api

--- src/api/books/books.router.js
+++ src/api/books/books.router.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import {
 	createBook,
 	getAllPublishedBooks,
-	getBookThumbnail,
 	getUserPublishedBooks,
 	searchBooksByTitle,
 	unpublishBookById
@@ -12,7 +11,7 @@ import upload from '../../utils/storage.js';
 const booksRouter = Router();
 
 booksRouter.route('/publish').post(upload.single('thumbnail'), createBook);
-booksRouter.route('/unpublish/:id').post(unpublishBookById);
+booksRouter.route('/unpublish/:id/').delete(unpublishBookById);
 booksRouter.route('/user').get(getUserPublishedBooks);
 booksRouter.route('/published').get(getAllPublishedBooks);
 booksRouter.route('/search').get(searchBooksByTitle);
